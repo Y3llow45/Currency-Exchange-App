e6b56de90336bee3a61835527120bfb33da94e9a
Борислав Христов <hristovborislav45@gmail.com>
2025-01-21 20:15:25 +0200

    Update GUI

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -8,15 +9,33 @@ import (
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Currency Exchange")
 
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
+	baseCurrency := widget.NewEntry()
+	baseCurrency.SetPlaceHolder("Enter base currency (e.g., USD)")
 
+	targetCurrency := widget.NewEntry()
+	targetCurrency.SetPlaceHolder("Enter target currency (e.g., EUR)")
+
+	amountEntry := widget.NewEntry()
+	amountEntry.SetPlaceHolder("Enter amount")
+
+	resultLabel := widget.NewLabel("Result will appear here")
+
+	convertButton := widget.NewButton("Convert", func() {
+		resultLabel.SetText("20 bucks")
+	})
+
+	content := container.NewVBox(
+		widget.NewLabel("Currency Exchange App"),
+		baseCurrency,
+		targetCurrency,
+		amountEntry,
+		convertButton,
+		resultLabel,
+	)
+
+	w.SetContent(content)
+	w.Resize(fyne.NewSize(400, 300))
 	w.ShowAndRun()
 }
