cbfad879f672cfe7c5cd4933dfe95b3b6d31b151
Борислав Христов <hristovborislav45@gmail.com>
2025-02-02 17:41:22 +0200

    Add load and save config functions for apiURL

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -16,11 +17,12 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-var apiURL = ""
+var apiURL string
 
 const (
 	dataFile   = "exchange_rates.json"
 	dateFormat = "2006-01-02"
+  configFile = "config.env"
 )
 
 type ExchangeRates struct {
@@ -28,15 +30,39 @@ type ExchangeRates struct {
 	TimeLastUpdateUnix int64             `json:"time_last_update_unix"`
 	BaseCode          string             `json:"base_code"`
 	ConversionRates   map[string]float64 `json:"conversion_rates"`
+  ApiURL            string             `json:"api_url"`
 }
 
 var history []string
-
 var (
 	errorLabel    *widget.Label
 	baseCurrency  *widget.Select
 	targetCurrency *widget.Select
+  data          *ExchangeRates
 )
+
+func LoadConfig() error {
+	fileData, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+      apiURL = ""
+			return ioutil.WriteFile(configFile, []byte("API_URL="), 0644)
+		}
+		return err
+	}
+	lines := strings.Split(string(fileData), "\n")
+	for _, line := range lines {
+		if strings.HasPrefix(line, "API_URL=") {
+			apiURL = strings.TrimPrefix(line, "API_URL=")
+			break
+		}
+	}
+	return nil
+}
+
+func SaveConfig() error {
+	return ioutil.WriteFile(configFile, []byte(fmt.Sprintf("API_URL=%s", apiURL)), 0644)
+}
 
 func addToHistory(from string, amount string, to string, converted float64) {
 	amountValue, _ := strconv.ParseFloat(amount, 64)
@@ -63,6 +89,7 @@ func FetchData() (*ExchangeRates, error) {
 	if data.Result != "success" {
 		return nil, fmt.Errorf("API request failed")
 	}
+  data.ApiURL = apiURL
 	return &data, nil
 }
 
@@ -83,6 +110,7 @@ func LoadData() (*ExchangeRates, error) {
 	if err := json.Unmarshal(fileData, &data); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal data: %w", err)
 	}
+  apiURL = data.ApiURL
 	return &data, nil
 }
 
@@ -111,6 +139,13 @@ func FetchAndSaveData() (*ExchangeRates, error) {
 }
 
 func SetData() *ExchangeRates {
+  LoadConfig()
+  if apiURL == "" {
+		errorLabel.SetText("Set API URL first")
+		errorLabel.Show()
+		return &ExchangeRates{ConversionRates: make(map[string]float64)}
+	}
+
 	data, err := CheckData()
 	if err != nil {
 		data, err = FetchAndSaveData()
@@ -139,6 +174,7 @@ func SetData() *ExchangeRates {
 func main() {
 	a := app.New()
 	w := a.NewWindow("Currency Exchange App")
+  LoadConfig()
 
 	apiURLEntry := widget.NewEntry()
 	apiURLEntry.SetPlaceHolder("Enter API URL")
@@ -153,20 +189,22 @@ func main() {
 	targetCurrency = widget.NewSelect([]string{}, func(value string) {})
 	targetCurrency.PlaceHolder = "Select target currency"
 
+  data = SetData()
+
 	toggleButton := widget.NewButton("Set API URL", func() {
 		if apiURLEntry.Visible() {
 			apiURLEntry.Hide()
 			newURL := apiURLEntry.Text
 			if newURL != "" {
 				apiURL = newURL
+        SaveConfig()
+        LoadConfig()
 				SetData()
 			}
 		} else {
 			apiURLEntry.Show()
 		}
 	})
-
-	data := SetData()
 
 	historyContainer := container.NewVBox()
 	for _, entry := range history {
